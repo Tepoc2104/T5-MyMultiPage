597d26a9a77518eddf5e5eac9f97e56499579596
Tepoc2104 <50753528+Tepoc2104@users.noreply.github.com>
2019-05-18 00:01:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var account = new Vue({
+    el: '#account',
+    data: {
+        message: "Наши подписчики",
+    }
+})
+var content = new Vue({
+    el: '#content',
+    data: {
+        message: "Every person , which doesnt like Game of Thrones Season 8 must subscribe our petiion for HBO channel, to make them redo final season of such beautiful history. We deserve much better than D&D gave us in final. To Subscribe, login|register, and click on the button below:"
+    }
+})
+var misc = new Vue({
+    el: '#misc',
+    data: {
+        message: 'Тут в планах быть контенту'
+    }
+})
